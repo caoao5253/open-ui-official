22084fe127a51d1d86d765eabcc1095ce5dc99c4
caoao5253 <caoao5253@163.com>
2023-03-10 18:02:08 +0800

    Signed-off-by: caoao5253 <caoao5253@163.com> 1.完成Layout-Head头部组件demo

--- src/Layout/Head/index.tsx
+++ src/Layout/Head/index.tsx
@@ -1,27 +1,52 @@
-import { Row,Col } from "antd"
-import { useState } from "react";
-import head from "./index.module.less"
+import Menu from '@/src/components/Menu'
+import { useCallback, useRef, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { whiteRoute } from '@/src/resource/whiteList'
+import head from './index.module.less'
+import route from '@/router/index'
+import { MenuProps } from '@/src/components/Menu/config'
+import { ReactComponent as Logo } from '@/src/assets/svg/github.svg'
+import { ItemEventType } from '@/src/components/Menu/MenuEvent'
+const items: MenuProps['item'] = []
+
+route.map((item, index) => {
+  //白名单直接跳过不进入导航菜单
+  if (whiteRoute.includes(item.path as string)) return
+  if (item.hideLayout) return
+  items.push({
+    label: item.title as string,
+    key: index.toString()
+  })
+})
 //网站头部组件封装
 const Head: React.FC = () => {
-  const [menuSelect, setmenuSelect] = useState<number>(0);
+  const [ab, seta] = useState<any>()
+  const navigate = useNavigate()
+  const uef = useRef(null)
+  const a: ItemEventType['onClick'] = useCallback((info: any) => {
+    seta(info)
+    console.log(uef.current, info)
+  }, [])
+  // let now = performance.now();
+  // while (performance.now() - now < 100) {}
+  // console.log("耗时的组件：Head render");
   return (
     <>
-      <header className={head["head-container"]}>
-        <div className={head["left-container"]}>
+      <header className={head['head-container']}>
+        <div className={head['left-container']}>
           <h1>Logo</h1>
         </div>
-        <div className={head["right-container"]}>
-          <div className={head["nav-search-wrapper"]}></div>
-          <ul className={head["open-menu-root"]}>
-            {/* <li className={head["open-menu-item"]}>组件</li> */}
-            <li className={menuSelect===0?`${head["open-menu-item"]} ${head["open-menu-item-select"]}` : head["open-menu-item"]} onClick={()=>setmenuSelect(0)}>博客</li>
-            <li className={menuSelect===1?`${head["open-menu-item"]} ${head["open-menu-item-select"]}` : head["open-menu-item"]} onClick={()=>setmenuSelect(1)}>国内镜像</li>
-          </ul>
+        <div className={head['right-container']}>
+          <div className={head['nav-search-wrapper']}></div>
+          <Menu ref={uef} item={items} onClick={a} mode='horizontal' />
+          <a href='https://github.com/caoao5253/open-ui' target='_blank' rel='noreferrer '>
+            <button className={head['btn-extend']} onClick={(e) => e.target}>
+              <span className={head['icon-extend']}>
+                <Logo />
+              </span>
+            </button>
+          </a>
         </div>
-        {/* <div className="open-row">
-        <div className={'  open-col-xs-24'}>dawdaw</div>
-        </div> */}
-       
       </header>
     </>
   )
--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,16 +1,47 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import route from "@/router/index"
 import MDX from "@/src/plugins"
-import React, { Suspense } from "react"
+import React, { Suspense, useRef } from "react"
 import Loading from "./Loading"
 import Head from "./Head"
+import { Menu, MenuProps } from "antd"
+type MenuItem = Required<MenuProps>['items'][number];
+function getItem(
+  label: React.ReactNode,
+  key?: React.Key | null,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  theme?: 'light' | 'dark',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    theme,
+  } as MenuItem;
+}
+const items: MenuItem[] = [
+  getItem(
+    'Navigation One',
+    'sub1',
+    'light',
+  ),
+  getItem('Option 5', '5'),
+  getItem('Option 6', '6'),
+];
 const Layout: React.FC = () => {
+  // const uef=useRef(null)
+  // const a=(...args: any[])=>{
+  //   console.log(uef.current)
+  //     console.log(args)
+  // }
   return (
     <>
-      <Head />
-      {/* <main></main> */}
-      {/* <BrowserRouter>
+    {/* <Menu onClick={a} items={items} ref={uef}/> */}
+      <BrowserRouter>
         <Suspense fallback={<Loading />}>
+          <Head />
           <Routes>
             {route.map((item) => {
               if (item.path === "/") {
@@ -20,9 +51,7 @@ const Layout: React.FC = () => {
             })}
           </Routes>
         </Suspense>
-
-      </BrowserRouter> */}
-      
+      </BrowserRouter>
     </>
   )
 }
