7628af8f75d0c9b3e85efdefa1f44c89803c966b
caoao5253 <caoao5253@163.com>
2023-03-10 17:59:18 +0800

    Signed-off-by: caoao5253 <caoao5253@163.com> 1.新增测试路由文件用于测试menu菜单

--- router/index.ts
+++ router/index.ts
@@ -4,17 +4,17 @@ import { PathRouteProps } from "react-router-dom";
  * 路由配置
  * @title       路由标题
  * @children    子路由
- * @isLayout    是否是Layout侧边栏菜单
+ * @hideLayout    是否是Layout侧边栏菜单
  * @redirect    重定向地址
  * @loader       在路由元素渲染前向其提供数据,该功能只有数据路由器才有效，例如createBrowserRouter、createMemoryRouter、createHashRouter等函数数据路由器才可以。loader?: NonIndexRouteObject["loader"]
  */
 interface routeConfig extends Omit<PathRouteProps, "children"> {
-  title: string;
+  title?: string;
   redirect?: string;
-  isLayout?: boolean;
+  hideLayout?: boolean;
   children?: Array<routeConfig>;
-
 }
+
 export default [
   {
     path: "/",
@@ -22,6 +22,23 @@ export default [
   },
   {
     path: "/home",
-    element:lazy(()=>import('@/src/docs/index.md'))
+    title: 'Open UI - 一套由个人开发者设计维护的React组件UI库',
+    element: lazy(() => import('@/src/docs/index.md')),
+    hideLayout:true
+  },
+  {
+    path: "/docs",
+    title:'研发',
+    element: lazy(() => import('@/src/docs/index.md')),
+  },
+  {
+    path: "/components",
+    title:'组件',
+    element: lazy(() => import('@/src/docs/index.md')),
+  },
+  {
+    path: "/blogger",
+    title:'博客',
+    element: lazy(() => import('@/src/docs/index.md')),
   }
-] as routeConfig[];
+]  as routeConfig[];
